f51565c635262a61e247cabe963ce811d3e63007
Samat <s.toichuev@innopolis.ru>
2022-05-28 18:40:17 +0600

    Add project files.

---
+++ TodoApp/Models/TodoModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    internal class TodoModel
+    {
+        private bool _isDone;
+        private string _text;
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+    }
+}
